e19783cdf6e8fdf48d1afd09868286004114e4f5
Barny Victor <victorbarny4@gmail.com>
2023-01-12 06:34:12 +0100

    added class validation

--- src/items/dto/create-item-dto.ts
+++ src/items/dto/create-item-dto.ts
@@ -1,5 +1,15 @@
+import { IsString, IsNumber, IsNotEmpty } from 'class-validator';
+
 export class CreateItemsDTO {
+  @IsNotEmpty()
+  @IsString()
   readonly name: string;
+
+  @IsNotEmpty()
+  @IsString()
   readonly description: string;
+
+  @IsNotEmpty()
+  @IsNumber()
   readonly qty: number;
 }
