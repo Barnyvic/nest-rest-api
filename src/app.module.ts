fe468f1d6202497925d3c0e8ed76bb0523f967fd
Barny Victor <victorbarny4@gmail.com>
2023-01-24 02:58:55 +0100

    feat: added report and users entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
+import { UserEntity } from './users/users.entity';
+import { ReportEntity } from './reports/reports.entity';
 
 @Module({
-  imports: [UsersModule, ReportsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      synchronize: true,
+      entities: [UserEntity, ReportEntity],
+      database: 'db.sqlite',
+    }),
+    UsersModule,
+    ReportsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/reports/reports.entity.ts
@@ -0,0 +1,9 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+@Entity()
+export class ReportEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  price: number;
+}
