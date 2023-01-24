f6a67b56b1ef2ae86b4b5055bec190c2192f29cd
Barny Victor <victorbarny4@gmail.com>
2023-01-24 02:09:23 +0100

    feat: started a new project mycv

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ItemsController } from './items/items.controller';
+import { UsersModule } from './users/users.module';
+import { ReportsModule } from './reports/reports.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, ItemsController],
+  imports: [UsersModule, ReportsModule],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
