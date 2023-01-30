cfeec16af798208688660aa15272280e76beee27
Barny Victor <victorbarny4@gmail.com>
2023-01-30 03:49:16 +0100

    removed the error from code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
@@ -20,7 +18,5 @@ import { AuthModule } from './auth/auth.module';
     ReportsModule,
     AuthModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
